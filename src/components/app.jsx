2f395d4477eef264dd29b40c334823e42f72fa76
Neal Bharucha <neal.barucha@msichicago.org>
2018-01-19 10:16:34 -0600

    task4: toggle views

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,14 +5,36 @@ import ItemAdd from './Item/ItemAdd';
 import ItemDisplay from './Item/ItemDisplay';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOrderView: false
+        }
+        this.modifyView = this.modifyView.bind(this);
+    }
+
+    modifyView () {
+        this.setState({
+            isOrderView: this.state.isOrderView ? false : true
+        })
+    }
+
     render () {
         return (
             <div>
                 <title>Order App</title>
-                <OrderAdd/>
-                <OrderDisplay/>
-                <ItemAdd/>
-                <ItemDisplay/>
+                <button onClick={() => this.modifyView()}>Toggle View</button>
+                {this.state.isOrderView ? (
+                    <div>
+                        <OrderAdd/>
+                        <OrderDisplay/> 
+                    </div>
+                ) : (
+                    <div>
+                        <ItemAdd/>
+                        <ItemDisplay/>
+                    </div>
+                )}
             </div>
         );
     }
